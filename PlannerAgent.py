bfcfaaea9c46ecb26e5669f90dc4fb511679d436
Tristin28 <tristin.bezzina.24@um.edu.mt>
2026-04-17 19:51:07 +0200

    Removed files from being standalone into src folder

--- PlannerAgent.py
+++
@@ -1,8 +0,0 @@
-from OllamaClient import OllamaClient
-
-class PlannerAgent():
-    def __init__(self, llm_client:OllamaClient):
-        self.llm_client = llm_client
-
-    def create_plan(self, task:str):
-        prompt = f" "
