2b3e11f43f26342af1a58e723cae5c187fd969ee
Tristin28 <tristin.bezzina.24@um.edu.mt>
2026-04-19 13:33:58 +0200

    Added temperature field

--- src/OllamaClient.py
+++ src/OllamaClient.py
@@ -9,7 +9,7 @@ class OllamaClient:
     def __init__(self, model: str = "qwen2.5:3b"):
         self.model = model
 
-    def invoke_text(self, messages: list[dict], stream) -> str:
+    def invoke_text(self, messages: list[dict], stream, options: dict) -> str:
         '''
             Each ask method will let the respective agent send its respective role based prompt (where it will be a list of dictionaries) to the LLM running on 
             the machine and then it receives a response back from the LLM in text and not in JSON format
@@ -18,12 +18,13 @@ class OllamaClient:
         response = chat(
             model=self.model,
             messages=messages,
-            stream=stream
+            stream=stream,
+            options = options
         )
 
         return response.message.content
     
-    def invoke_json(self,messages: list[dict], stream, schema: dict) -> dict:
+    def invoke_json(self,messages: list[dict], stream, schema: dict, options: dict) -> dict:
         '''
             Creating a new method which is the same as invoke_text but returns response in JSON structured format defined by the respective agent but still text 
             however loads method will turn it into a dict object so planner agent can make use of it, so that no encoding of 
@@ -35,5 +36,8 @@ class OllamaClient:
             messages=messages,
             stream=stream,
             format=schema,
+            options = {
+                "temperature": 0.1 #A paramater which controls how strongly the model prefers the most probable next token over the other possible tokens
+            }
         )
         return json.loads(response.message.content)
